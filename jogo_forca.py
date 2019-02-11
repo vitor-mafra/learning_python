6283858c4288ff90749ca3e4ab37d66ce2f86862
Vitor  Mafra <vitor.mafra@hotmail.com>
2019-02-11 15:49:35 -0200

    Add files via upload
    Inicio do jogo de forca

---
+++ jogo_forca.py
@@ -0,0 +1,12 @@
+def jogar():
+
+	print("\n*********************************")
+	print("***Bem vindo ao jogo da Forca!***")
+	print("*********************************\n")
+
+
+
+	print("Fim do jogo!")
+
+if(__name__ == "__main__"):
+	jogar()
